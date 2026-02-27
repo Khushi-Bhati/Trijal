bef40317fb73abf6d1c33c12a7ddd58c9242dd13
Khushi Bhati <khushibhati2413@gmail.com>
2026-02-27 19:51:44 +0530

    Update Contact.js

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ContactSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
-    lastName: { type: String,required:true },
+    designation:{type:String, required:true},
     email: { type: String, required: true },
     phone: { type: String , required:true},
  
